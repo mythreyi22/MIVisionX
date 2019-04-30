9434f27db83d0e7da201e79f347bb99d0d642f79
Mythreyi <mythreyi@multicorewareinc.com>
2019-04-30 16:47:25 +0530

    Add support for vxExtrppNode_brightness
    This create the an node for rpp brightness and contrast function.

---
+++ amd_openvx_extensions/amd_rpp/include/kernels_rpp.h
@@ -0,0 +1,51 @@
+/*
+
+MCW
+
+
+*/
+
+
+
+#ifndef _VX_KERNELS_RPP_H_
+#define _VX_KERNELS_RPP_H_
+
+//////////////////////////////////////////////////////////////////////
+// SHARED_PUBLIC - shared sybols for export
+// STITCH_API_ENTRY - export API symbols
+#if _WIN32
+#define SHARED_PUBLIC extern "C" __declspec(dllexport)
+#else
+#define SHARED_PUBLIC extern "C" __attribute__ ((visibility ("default")))
+#endif
+
+//////////////////////////////////////////////////////////////////////
+// common header files
+#include <VX/vx.h>
+#include <vx_ext_rpp.h>
+#include <vx_ext_amd.h>
+#include <iostream>
+#include <string.h>
+
+#define VX_LIBRARY_RPP         1
+
+enum vx_kernel_ext_amd_rpp_e
+
+{
+    VX_KERNEL_BRIGHTNESS_AND_CONTRAST  = VX_KERNEL_BASE(VX_ID_MCW, VX_LIBRARY_RPP) + 0x001,
+};
+
+//////////////////////////////////////////////////////////////////////
+//! \brief Common data shared across all nodes in a graph
+/*struct NeuralNetworkCommonHandle {
+    int count;
+    miopenHandle_t  miopen_handle;
+    cl_command_queue cmdq;
+    bool exhaustiveSearch;
+};
+*/
+//////////////////////////////////////////////////////////////////////
+//! \brief The utility functions
+vx_node createNode(vx_graph graph, vx_enum kernelEnum, vx_reference params[], vx_uint32 num);
+
+#endif //__KERNELS_H__
---
+++ amd_openvx_extensions/amd_rpp/source/kernel_rpp.cpp
@@ -0,0 +1,65 @@
+#include "kernels_rpp.h"
+
+VX_API_ENTRY vx_node VX_API_CALL vxExtrppNode_brightness(vx_graph graph, vx_array pSrc, vx_array pDst, vx_int32  height, vx_int32 width, vx_float32 alpha, vx_float32 beta)
+{
+    vx_node node = NULL;
+    vx_context context = vxGetContext((vx_reference)graph);
+    if(vxGetStatus((vx_reference)context) == VX_SUCCESS) {
+        vx_scalar ALPHA = vxCreateScalar(vxGetContext((vx_reference)graph), VX_TYPE_FLOAT32, &alpha);
+        vx_scalar BETA = vxCreateScalar(vxGetContext((vx_reference)graph), VX_TYPE_FLOAT32, &alpha);
+        vx_scalar HEIGHT = vxCreateScalar(vxGetContext((vx_reference)graph), VX_TYPE_FLOAT32, &alpha);
+        vx_scalar WIDTH = vxCreateScalar(vxGetContext((vx_reference)graph), VX_TYPE_FLOAT32, &alpha);
+
+            vx_reference params[] = {
+                (vx_reference) pSrc,
+                (vx_reference) pDst,
+                (vx_reference) HEIGHT,
+                (vx_reference) WIDTH,
+                (vx_reference) ALPHA,
+                (vx_reference) BETA
+        };
+            node = createNode(graph, VX_KERNEL_BRIGHTNESS_AND_CONTRAST, params, 6);
+    }
+    return node;
+}
+
+// utility functions
+vx_node createNode(vx_graph graph, vx_enum kernelEnum, vx_reference params[], vx_uint32 num)
+{
+    vx_status status = VX_SUCCESS;
+    vx_node node = 0;
+    vx_context context = vxGetContext((vx_reference)graph);
+    if(vxGetStatus((vx_reference)context) != VX_SUCCESS) {
+        return NULL;
+    }
+    vx_kernel kernel = vxGetKernelByEnum(context, kernelEnum);
+    if(vxGetStatus((vx_reference)kernel) == VX_SUCCESS) {
+        node = vxCreateGenericNode(graph, kernel);
+        if (node) {
+            vx_uint32 p = 0;
+            for (p = 0; p < num; p++) {
+                if (params[p]) {
+                    status = vxSetParameterByIndex(node, p, params[p]);
+                    if (status != VX_SUCCESS) {
+                        char kernelName[VX_MAX_KERNEL_NAME];
+                        vxQueryKernel(kernel, VX_KERNEL_NAME, kernelName, VX_MAX_KERNEL_NAME);
+                        vxAddLogEntry((vx_reference)graph, status, "createNode: vxSetParameterByIndex(%s, %d, 0x%p) => %d\n", kernelName, p, params[p], status);
+                        vxReleaseNode(&node);
+                        node = 0;
+                        break;
+                    }
+                }
+            }
+        }
+        else {
+            vxAddLogEntry((vx_reference)graph, VX_ERROR_INVALID_PARAMETERS, "createNode: failed to create node with kernel enum %d\n", kernelEnum);
+            status = VX_ERROR_NO_MEMORY;
+        }
+        vxReleaseKernel(&kernel);
+    }
+    else {
+        vxAddLogEntry((vx_reference)graph, VX_ERROR_INVALID_PARAMETERS, "createNode: failed to retrieve kernel enum %d\n", kernelEnum);
+        status = VX_ERROR_NOT_SUPPORTED;
+    }
+    return node;
+}
