90f2419588f443aedd726b97e7395171b578b496
Your Name <you@example.com>
2019-04-30 16:41:15 +0530

    Commit to freeze framework

--- amd_openvx_extensions/amd_rpp/include/vx_ext_rpp.h
+++ amd_openvx_extensions/amd_rpp/include/vx_ext_rpp.h
@@ -11,12 +11,13 @@ MCW
 #define _VX_EXT_RPP_H_
 
 #include <VX/vx.h>
+#include "kernels_rpp.h"
 
 
 	/*!***********************************************************************************************************
 						RPP VX_API_ENTRY C Function NODE
 	*************************************************************************************************************/
 
-extern "C" SHARED_PUBLIC vx_node VX_API_CALL vxExtrppNode_brightness(vx_graph graph, vx_array in, vx_array out, vx_float32 alpha, vx_float32 beta);
+extern "C" SHARED_PUBLIC vx_node VX_API_CALL vxExtrppNode_brightness(vx_graph graph, vx_array pSrc, vx_array pDst, vx_int32  height, vx_int32 width, vx_float32 alpha, vx_float32 beta);
 
-#endif 
+#endif
